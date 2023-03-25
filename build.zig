5db15b56f9312f6df45f93ec55cf1e6f12f86a89
Kyle McIntosh <kylemcintosh3@hotmail.co.uk>
2023-03-25 15:21:00 +0000

    dynamic and static linking options

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
@@ -10,16 +11,30 @@ pub fn build(b: *std.Build) !void {
         .target = target,
     });
 
-    exe.addVcpkgPaths(.dynamic) catch {};
-    if (exe.vcpkg_bin_path) |path| {
-        std.debug.print("Using : vcpkg and linkSystemLibraryName(\"sdl\")\n", .{});
+    if (builtin.cpu.arch == .x86_64 and builtin.os.tag == .windows) {
+        exe.addVcpkgPaths(.dynamic) catch {};
+        if (exe.vcpkg_bin_path) |path| {
+            std.debug.print("Using : vcpkg and linkSystemLibraryName(\"sdl\")\n", .{});
 
-        // we found SDL2 in vcpkg, just install and use this variant
-        const src_path = std.fs.path.join(b.allocator, &.{ path, "SDL2.dll" }) catch @panic("out of memory");
-        std.debug.print("src_path : {s}\n", .{src_path});
+            // we found SDL2 in vcpkg, just install and use this variant
+            const src_path = std.fs.path.join(b.allocator, &.{ path, "SDL2.dll" }) catch @panic("out of memory");
+            std.debug.print("src_path : {s}\n", .{src_path});
 
-        exe.linkSystemLibraryName("sdl2");
-        b.installBinFile(src_path, "SDL2.dll");
+            exe.linkSystemLibraryName("sdl2");
+            b.installBinFile(src_path, "SDL2.dll");
+        } else {
+            // Linking dynamically
+            //exe.addIncludePath("deps/include/SDL2");
+            //exe.addLibraryPath("deps/lib/x64");
+            //exe.linkSystemLibraryName("sdl2");
+            //exe.install();
+
+            // Linking statically
+            //exe.linkLibC();
+            //exe.addIncludePath("deps/include/SDL2");
+            //exe.addObjectFile("deps/lib/x64/SDL2.lib");
+            //exe.install();
+        }
     } else {
         std.debug.print("Using : linkSystemLibrary(\"sdl\")\n", .{});
         exe.linkSystemLibrary("sdl2");
